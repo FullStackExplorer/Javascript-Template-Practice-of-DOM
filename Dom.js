203641c599c0640f229bc4bce59eb4ef6db590c9
Muhammad Uzair Younas <143187164+FullStackExplorer@users.noreply.github.com>
2023-08-28 17:55:50 +0500

    Add files via upload

---
+++ Dom.js
@@ -0,0 +1,53 @@
+let dell;
+dell = document.all;
+console.log(dell);
+
+
+let n;
+n = document.documentElement;
+console.log(n);
+
+
+
+
+let a;
+a = document.head;
+console.log(a);
+
+
+let b;
+b = document.title;
+console.log(b);
+
+
+let c;
+c = document.body;
+console.log(c);
+
+let d;
+d = document.images;
+console.log(d);
+
+let e;
+e = document.anchors;
+console.log(e);
+
+let links;
+links = document.links;
+console.log(links);
+
+let form;
+form = document.forms;
+console.log(form);
+
+let url;
+url = document.URL;
+console.log(url);
+
+let base;
+base = document.baseURI;
+console.log(base);
+
+var dom;
+dom = document.domain;
+console.log(dom);
